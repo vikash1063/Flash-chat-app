587fae9270135683912269b17c71a356f16df1ff
barrytra <BHAVESHGUPTA1602@GMAIL.COM>
2024-01-05 19:47:41 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const workoutRoutes = require("./routes/workouts")
+
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req,res,next) => {
+    console.log(req.path, req.method)
+    next();
+})
+
+// routes
+app.use('api/workouts', workoutRoutes)
+
+// listen
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
+})
+
+
+
+

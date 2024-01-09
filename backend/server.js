111b5c0e97132da33b6a1531311c38c000f623f5
barrytra <BHAVESHGUPTA1602@GMAIL.COM>
2024-01-09 14:20:49 +0530

    deployment code added

--- backend/server.js
+++ backend/server.js
@@ -4,31 +4,51 @@ const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const {notFound, errorHandler} = require("./middleware/errorMiddleware")
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
-dotenv.config()
-
+dotenv.config();
 connectDB();
+const app = express();
 
-const app = express()
+app.use(express.json()); // to accept json data
 
-app.get('/', () => {
-    res.send("API running successfully")
-})
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
-// middleware
-app.use(express.json())
-app.use('/api/user', userRoutes)
-app.use('/api/chat', chatRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
-app.use(notFound)
-app.use(errorHandler)
 
-// listen
-const PORT = process.env.PORT || 5000
-const server = app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+    });
+}
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+
+const server = app.listen(
+    PORT,
+    console.log(`Server running on PORT ${PORT}...`)
+);
 
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
@@ -69,6 +89,3 @@ io.on("connection", (socket) => {
         socket.leave(userData._id);
     });
 });
-
-
-
